2c7acd67cb2ac1e18e277a12bfc0ef3cddab3f94
Michael <michael@managedkaos.com>
2018-08-14 02:47:39 -0700

    Remove custom config

--- provision.sh
+++ provision.sh
@@ -4,14 +4,5 @@ apt-get update
 # Install nginx and supporting packages
 apt-get install -y nginx
 
-# Remove the default configuration
-unlink /etc/nginx/sites-enabled/default
-
-# Install the new configuration
-cp /vagrant/upstreams.conf /etc/nginx/conf.d
-
-# Start the app servers
-/usr/bin/python3 /vagrant/start_app_servers.py &
-
 # Load the configuration
 systemctl reload nginx
